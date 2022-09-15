4ae2b5c9ff03b7623f263a68f85573dde5fbdd70
reeveng <govaertreeven@gmail.com>
2022-09-16 00:07:09 +0200

    rename avatarButton to profileMenu

---
+++ src/components/Header/partials/ProfileMenu.js
@@ -0,0 +1,33 @@
+import {
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+
+export const ProfileMenu = ({ name, src }) => {
+  return (
+    <Menu>
+      <MenuButton
+        as="button"
+        transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+        borderRadius="full"
+        _focus={{
+          boxShadow:
+            "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+        }}
+      >
+        <Avatar name={name} src={src} />
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>Inbox</MenuItem>
+        <MenuItem>Preferences</MenuItem>
+        <MenuItem>Sign out</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,15 @@ import { Hero } from "../components/Hero";
 import { Container } from "../components/Container";
 import { Main } from "../components/Main";
 import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
+import { Header } from "../components/Header/Header";
 import { Button } from "@chakra-ui/react";
-import { AvatarButton } from "../components/Buttons/AvatarButton";
+import { ProfileMenu } from "../components/Header/partials/ProfileMenu";
 
 const Index = () => (
   <Container height="100vh">
     <Header>
       <Button>IconDing</Button>
-      <AvatarButton name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+      <ProfileMenu name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
     </Header>
     <Hero />
     <Main></Main>
