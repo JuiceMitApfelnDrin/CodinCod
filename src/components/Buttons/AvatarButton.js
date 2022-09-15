775f065ad48c667a8470e213952e5efce94dfce1
reeveng <govaertreeven@gmail.com>
2022-09-15 23:14:30 +0200

    cleanup of header => left typescript props in there and reworked avatarbutton

--- src/components/Buttons/AvatarButton.js
+++ src/components/Buttons/AvatarButton.js
@@ -1,9 +1,17 @@
-import { Avatar, Button } from "@chakra-ui/react";
+import { Avatar, Box } from "@chakra-ui/react";
 
-export const AvatarButton = () => {
+export const AvatarButton = ({ name, src }) => {
   return (
-    <Button borderRadius="full">
-      <Avatar />
-    </Button>
+    <Box
+      as="button"
+      transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+      borderRadius="full"
+      _focus={{
+        boxShadow:
+          "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+      }}
+    >
+      <Avatar name={name} src={src} />
+    </Box>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Flex, FlexProps } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 export const Header = (props) => (
   <Flex
