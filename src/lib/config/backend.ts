33f0b1c1cb50e08036c1bc946ea0d6b5481c0a81
J Mad <36441093+reeveng@users.noreply.github.com>
2024-09-08 01:25:26 +0200

    feat: throw error when missing env backendurl

--- src/lib/config/backend.ts
+++ src/lib/config/backend.ts
@@ -15,7 +15,13 @@ interface Params {
  * @returns the desired url
  */
 export function buildBackendUrl(path: BackendUrl, params: Params = {}) {
-	let url = `${import.meta.env.VITE_BACKEND_URL}${path}`;
+	const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+	if (!backendUrl) {
+		throw new Error("Bruh, you forgot to add vite_backend_url to your .env");
+	}
+
+	let url = `${backendUrl}${path}`;
 
 	if (params) {
 		// Replace placeholders in the path with actual values
