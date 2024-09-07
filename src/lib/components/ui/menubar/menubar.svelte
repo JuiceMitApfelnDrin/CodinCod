555221e988f85c3f797974cd34a9d79f82d9dedd
J Mad <36441093+reeveng@users.noreply.github.com>
2024-09-08 01:17:20 +0200

    feat: add menubar and sub components from shadcn
    executed a shadcn command to get these components

---
+++ src/lib/components/ui/menubar/menubar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from "@/utils/cn";
+	import { Menubar as MenubarPrimitive } from "bits-ui";
+
+	type $$Props = MenubarPrimitive.Props;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<MenubarPrimitive.Root
+	class={cn("bg-background flex h-10 items-center space-x-1 rounded-md border p-1", className)}
+	{...$$restProps}
+>
+	<slot />
+</MenubarPrimitive.Root>
