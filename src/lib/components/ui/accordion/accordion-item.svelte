3856b4111b21f6181a725680ace6afe96d518111
J Mad <36441093+reeveng@users.noreply.github.com>
2024-09-11 00:31:39 +0200

    feat: add shadcn components
    add accordion from shadcn and add separator from shadcn

---
+++ src/lib/components/ui/accordion/accordion-item.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { cn } from "@/utils/cn";
+	import { Accordion as AccordionPrimitive } from "bits-ui";
+
+	type $$Props = AccordionPrimitive.ItemProps;
+
+	let className: $$Props["class"] = undefined;
+	export let value: $$Props["value"];
+	export { className as class };
+</script>
+
+<AccordionPrimitive.Item {value} class={cn("border-b", className)} {...$$restProps}>
+	<slot />
+</AccordionPrimitive.Item>
