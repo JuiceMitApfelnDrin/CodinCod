915487daa5044cff3247058108e59e97812d49ae
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2024-09-16 00:05:27 +0200

    cleaup: remove jwtpayloadschema
    changed name to authenticated user info schema

--- src/schemas/authentication/jwt-payload.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { userEntitySchema } from "../user/user-entity.schema.js";
-
-export const jwtPayloadSchema = z.object({
-	userId: z.string(),
-	username: userEntitySchema.shape.username
-});
-export type JwtPayload = z.infer<typeof jwtPayloadSchema>;
