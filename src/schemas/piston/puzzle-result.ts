ba101a045f13f937ad6052b2bd3e191f7a8d8f40
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2024-09-22 23:07:30 +0200

    docs: add a TODO about puzzleresult.unknown
    we should consider removing it eventually, since all results should be
    known and we should give good feedback to our users

--- src/schemas/piston/puzzle-result.ts
+++ src/schemas/piston/puzzle-result.ts
@@ -4,6 +4,8 @@ import { PuzzleResultEnum } from "../../enums/puzzle-result-enum.js";
 export const puzzleResultSchema = z.enum([
 	PuzzleResultEnum.ERROR,
 	PuzzleResultEnum.SUCCESS,
+
+	// TODO: probably want to get rid of unknown situations eventually :)
 	PuzzleResultEnum.UNKNOWN
 ]);
 export type PuzzleResult = z.infer<typeof puzzleResultSchema>;
