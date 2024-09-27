aa67c6a080f3853853987492fe5b04208948c1f3
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2024-09-27 23:47:06 +0200

    refactor: error component, made it a card
    which looks cuter UwU

--- libs/frontend/src/lib/components/error/error.svelte
+++ libs/frontend/src/lib/components/error/error.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { frontendUrls } from "types";
 	import Container from "../ui/container/container.svelte";
+	import * as Card from "../ui/card";
+	import H1 from "../typography/h1.svelte";
+	import P from "../typography/p.svelte";
+	import Button from "../ui/button/button.svelte";
 
 	export let status: number = 500;
 	export let message: string | null = null;
@@ -8,6 +12,7 @@
 
 	type ErrorTypeDefaultInfo = { header: string; description: string };
 	const errorMap: Record<number, ErrorTypeDefaultInfo> = {
+		401: { description: "You are not welcome here. You shall not pass!", header: "Unauthorized" },
 		403: { description: "You don't have permission to access this page.", header: "Forbidden" },
 		404: { description: "The page you are looking for does not exist.", header: "Page Not Found" },
 		500: {
@@ -23,8 +28,18 @@
 </script>
 
 <Container>
-	<h1 class="text-4xl font-bold">{status} - {header ?? errorInfo.header}</h1>
-	<p class="text-lg">{message ?? errorInfo.description}</p>
+	<Card.Root class="mx-auto mt-4 md:mt-8 md:max-w-lg">
+		<Card.Header>
+			<H1>
+				{status} - {header ?? errorInfo.header}
+			</H1>
+		</Card.Header>
+		<Card.Content>
+			<P class="text-lg">{message ?? errorInfo.description}</P>
+		</Card.Content>
 
-	<a href={frontendUrls.ROOT} class="mt-4 text-blue-600">Go to Homepage</a>
+		<Card.Footer>
+			<Button href={frontendUrls.ROOT} class="mt-4">Go to Homepage</Button>
+		</Card.Footer>
+	</Card.Root>
 </Container>
