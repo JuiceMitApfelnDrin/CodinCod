939adf5a53e07322c18625708a05414d9eb9c435
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2024-10-14 22:04:20 +0200

    feat: introduction of a logical unit
    it's purpose is to surround elements that logically belong together.
    
    ---
    
    Yes, i hear you, a simple div could do the same, this basically is a
    simple div that you can give some class.
    And you would be fully right! But this element has a semantic benefit,
    by declaring it as a logical element, we also read it is as such. The
    intention of this component is not to replace the infamous `div`.
    This component exists to group things, and create a better understanding
    of which elements really belong together and aren't there for
    spacing/beautifying stuff necessarily :), welcome to my ted talk.

---
+++ libs/frontend/src/lib/components/ui/logical-unit/logical-unit.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from "svelte/elements";
+
+	type $$Props = HTMLInputAttributes;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<!-- important: should never have any classes defined by itself! -->
+<div class={className}>
+	<slot />
+</div>
