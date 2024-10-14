dcf2162c8aa80eada60572129887c753f3ebcf89
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2024-10-14 22:15:12 +0200

    refactor: add more/less spacing on container
    previously a lot of elements would add their own spacing, this
    consolidates that logic in the most logical place, since every page
    should be surrounded by a container element

--- libs/frontend/src/lib/components/ui/container/container.svelte
+++ libs/frontend/src/lib/components/ui/container/container.svelte
@@ -8,6 +8,11 @@
 	export { className as class };
 </script>
 
-<div class={cn("container mx-auto my-4 flex max-w-screen-lg flex-col", className)}>
-	<slot></slot>
+<div
+	class={cn(
+		"container mx-auto flex max-w-screen-lg flex-col gap-4 py-4 md:gap-8 md:py-8 lg:gap-12 xl:py-12",
+		className
+	)}
+>
+	<slot />
 </div>
