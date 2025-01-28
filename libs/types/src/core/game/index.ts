9c84030e666013aedf8b3461a077787af2a0a0af
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-01-28 22:44:19 +0100

    refactor: add forgotten import/export

--- libs/types/src/core/game/index.ts
+++ libs/types/src/core/game/index.ts
@@ -11,6 +11,7 @@ export * from "./schema/options.schema.js";
 export * from "./schema/game-user-info.schema.js";
 export * from "./schema/visibility.schema.js";
 export * from "./schema/game-entity.schema.js";
+export * from "./schema/game-dto.schema.js";
 export * from "./schema/mode.schema.js";
 export * from "./schema/game-event.schema.js";
 
