7baf8a78eb230edf1445e05e213c03acbbb5d236
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-01-28 21:51:58 +0100

    feat: add markdown component that handles xss attacks

--- libs/frontend/src/lib/components/typography/markdown.svelte
+++ libs/frontend/src/lib/components/typography/markdown.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
 	import { marked } from "marked";
+	import { onMount } from "svelte";
+	import P from "./p.svelte";
+	import DOMPurify from "dompurify";
 
 	export let markdown: string | null = null;
 	export let fallbackText: string = "no fallback provided";
+
+	let parsedMarkdown: string | null = null;
+	let isLoading: boolean = true;
+
+	const parseMarkdown = async () => {
+		if (markdown) {
+			const dirtyMarkdown = await marked.parse(markdown);
+			parsedMarkdown = DOMPurify.sanitize(dirtyMarkdown);
+		}
+		isLoading = false;
+	};
+
+	// Run the async parsing when component is mounted or markdown changes
+	onMount(() => {
+		parseMarkdown();
+	});
 </script>
 
-{#if markdown}
-	<div class="prose dark:prose-invert prose-stone">
-		{@html marked.parse(markdown)}
+{#if isLoading}
+	<P>Loading...</P>
+{:else if parsedMarkdown}
+	<div class="prose prose-stone dark:prose-invert">
+		{@html parsedMarkdown}
 	</div>
 {:else}
 	<p>{fallbackText}</p>
