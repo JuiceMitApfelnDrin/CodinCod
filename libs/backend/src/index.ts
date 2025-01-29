46f7c654193ebcbc2037721e2b2dc36b0d908fae
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-01-29 22:10:22 +0100

    fix: docker-compose build

--- libs/backend/src/index.ts
+++ libs/backend/src/index.ts
@@ -1,13 +1,14 @@
 import server from "./app.js";
 
-const FASTIFY_PORT = Number(process.env.PORT) || 8888;
+const FASTIFY_HOST = process.env.FASTIFY_HOST || "0.0.0.0";
+const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 8888;
 
 // start server
-server.listen({ port: FASTIFY_PORT }, (err) => {
+server.listen({ port: FASTIFY_PORT, host: FASTIFY_HOST }, (err, address) => {
 	if (err) {
 		server.log.error(err);
 		process.exit(1);
 	}
-});
 
-console.log(`🚀 Fastify server running on port http://localhost:${FASTIFY_PORT}`);
+	console.log(`🚀 Fastify server running on port ${address}`);
+});
