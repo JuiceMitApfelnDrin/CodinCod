717c26197afe0f6b631ff093243f11e386ea4eec
Andriamanitra <10672443+Andriamanitra@users.noreply.github.com>
2025-02-04 10:45:23 +0000

    Apply automatic changes

--- libs/frontend/src/routes/+page.svelte
+++ libs/frontend/src/routes/+page.svelte
@@ -5,21 +5,30 @@
 </script>
 
 <Container>
-	<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight max-w-[55ch]">
-	CodinCod is a website for solving programming puzzles either on your own or while competing
-	against your friends and enemies.
+	<h3 class="max-w-[55ch] scroll-m-20 text-2xl font-semibold tracking-tight">
+		CodinCod is a website for solving programming puzzles either on your own or while competing
+		against your friends and enemies.
 	</h3>
 
 	<P class="max-w-[75ch]">
-	This site is under construction &mdash; if you'd like to help check out our <a href="https://github.com/JuiceMitApfelnDrin/CodinCod">Github repository</a>!
-	All kinds of contributions (issues, pull requests, general feedback and ideas) are welcome! You may also participate by chatting on <a href="https://twitch.tv/JuiceMitApfelnDrin">twitch.tv</a> where this project is being coded live.
+		This site is under construction &mdash; if you'd like to help check out our <a
+			href="https://github.com/JuiceMitApfelnDrin/CodinCod">Github repository</a
+		>! All kinds of contributions (issues, pull requests, general feedback and ideas) are welcome!
+		You may also participate by chatting on
+		<a href="https://twitch.tv/JuiceMitApfelnDrin">twitch.tv</a> where this project is being coded live.
 	</P>
 
 	<P class="max-w-[75ch]">
-		<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight max-w-[55ch]">When (DEFINITELY <b>NOT</b> if) CodinCod is done you will be able to:</h3>
+		<h3 class="max-w-[55ch] scroll-m-20 text-2xl font-semibold tracking-tight">
+			When (DEFINITELY <b>NOT</b> if) CodinCod is done you will be able to:
+		</h3>
 
 		<ul class="list-inside list-disc space-y-2">
-			<li>Compete against others in real time (similar to <a href="https://www.codingame.com/multiplayer/clashofcode">Clash of Code</a>)</li>
+			<li>
+				Compete against others in real time (similar to <a
+					href="https://www.codingame.com/multiplayer/clashofcode">Clash of Code</a
+				>)
+			</li>
 			<li>Learn the basics of programming</li>
 			<li>Solve puzzles on your own without time pressure (code golf, performance optimization)</li>
 			<li>Contribute your own puzzles</li>
