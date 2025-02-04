2aded2911fa4f103db68978e0641e5e534f4025d
Andriamanitra <mikko77792@gmail.com>
2025-02-04 11:26:04 +0200

    simpler nav, /logout route for logging out

--- libs/frontend/src/routes/+layout.server.ts
+++ libs/frontend/src/routes/+layout.server.ts
@@ -1,16 +1,8 @@
-import { searchParamKeys } from "@/config/search-params.js";
 import { getAuthenticatedUserInfo } from "@/features/authentication/utils/get-authenticated-user-info.js";
-import { logout } from "@/features/authentication/utils/logout.js";
 import type { ServerLoadEvent } from "@sveltejs/kit";
 import { cookieKeys } from "types";
 
 export async function load({ cookies, fetch, url }: ServerLoadEvent) {
-	const isLoggingOut = url.search.includes(searchParamKeys.LOGOUT);
-
-	if (isLoggingOut) {
-		logout(cookies);
-	}
-
 	const token = cookies.get(cookieKeys.TOKEN);
 
 	if (!token) {
