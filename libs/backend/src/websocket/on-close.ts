f510150be27ad2959769febee3ebeef60e37b3de
J Mad <36441093+reeveng@users.noreply.github.com>
2025-02-05 02:20:25 +0000

    Update libs/backend/src/websocket/on-close.ts

--- libs/backend/src/websocket/on-close.ts
+++ libs/backend/src/websocket/on-close.ts
@@ -2,7 +2,6 @@ import { WebSocket } from "@fastify/websocket";
 import { removePlayerFromPlayers } from "./remove-player-from-players.js";
 import { removeStoppedPlayersFromGames } from "./remove-player-from-games.js";
 import { OpenGames } from "@/types/games.js";
-import { removeEmptyGames } from "./remove-empty-games.js";
 
 export async function onClose({
 	players,
