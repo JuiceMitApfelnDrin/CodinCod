2a521bc9ce54cd49b0becf36c305264f791a2a9f
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-02-05 03:03:23 +0100

    fix: game is nullable, check whether game exists

--- libs/backend/src/types/games.ts
+++ libs/backend/src/types/games.ts
@@ -3,4 +3,4 @@ import { GameUserInfo } from "types";
 export type Username = string;
 export type GameId = string;
 export type OpenGame = { [key: Username]: GameUserInfo };
-export type OpenGames = { [key: GameId]: OpenGame };
+export type OpenGames = { [key: GameId]: OpenGame | null };
--- libs/backend/src/websocket/waiting-room/update-players.ts
+++ libs/backend/src/websocket/waiting-room/update-players.ts
@@ -2,10 +2,16 @@ import { OpenGames } from "@/types/games.js";
 import { WebSocket } from "@fastify/websocket";
 import { updatePlayer } from "../common/update-player.js";
 import { GameEventEnum } from "types";
+import { removeGameFromGames } from "./remove-game-from-games.js";
 
 export function updatePlayers({ sockets, games }: { sockets: WebSocket[]; games: OpenGames }) {
-	const joinableGames = Object.entries(games).map(([key, value]) => {
-		return { id: key, amountOfPlayersJoined: Object.keys(value).length };
+	const joinableGames = Object.entries(games).map(([key, game]) => {
+		if (!game) {
+			removeGameFromGames({ gameId: key, games });
+			return { id: key, amountOfPlayersJoined: 0 };
+		}
+
+		return { id: key, amountOfPlayersJoined: Object.keys(game).length };
 	});
 
 	for (const socket of sockets) {
