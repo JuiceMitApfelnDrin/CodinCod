dc2a4a8689e9f96e9357c058b7e21c5e3c3644a9
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-02-07 23:51:19 +0100

    refactor: rename variable

--- libs/frontend/src/routes/puzzles/[id]/+page.server.ts
+++ libs/frontend/src/routes/puzzles/[id]/+page.server.ts
@@ -6,8 +6,8 @@ export async function load({ fetch, params }: PageServerLoadEvent) {
 	const id = params.id;
 	const url = buildBackendUrl(backendUrls.PUZZLE_DETAIL, { id });
 
-	const res = await fetch(url);
-	const puzzle = await res.json();
+	const response = await fetch(url);
+	const puzzle = await response.json();
 
 	return { puzzle };
 }
