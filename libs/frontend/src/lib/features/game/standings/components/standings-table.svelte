cde3b0d153b4892c5a9a75cfa6603d63c782261b
J Mad <36441093+reeveng@users.noreply.github.com>
2025-02-08 04:15:55 +0000

    Add hh mm ss to standings table time instead of just when submitted (#51)
    * chore: install duration plugin for dayjs
    
    * fix: update the standing table to show hh:mm:ss
    
    * docs: add todo comment

--- libs/frontend/src/lib/features/game/standings/components/standings-table.svelte
+++ libs/frontend/src/lib/features/game/standings/components/standings-table.svelte
@@ -2,6 +2,8 @@
 	import * as Table from "$lib/components/ui/table";
 	import dayjs from "dayjs";
 	import { isSubmissionDto, isUserDto, type GameDto, type SubmissionDto } from "types";
+	import duration from "dayjs/plugin/duration";
+	dayjs.extend(duration);
 
 	export let game: GameDto;
 
@@ -15,20 +17,21 @@
 			<Table.Row>
 				<Table.Head>User</Table.Head>
 				<Table.Head>Language</Table.Head>
-				<Table.Head>Submitted at</Table.Head>
+				<Table.Head>Time to solve</Table.Head>
 				<Table.Head>Result</Table.Head>
 			</Table.Row>
 		</Table.Header>
 		<Table.Body>
-			{#each playerSubmissions as submission}
-				{#if isUserDto(submission.user)}
+			{#each playerSubmissions as { user, language, createdAt, result }}
+				{#if isUserDto(user)}
 					<Table.Row>
-						<Table.Cell>{submission.user.username}</Table.Cell>
-						<Table.Cell>{submission.language}</Table.Cell>
+						<Table.Cell>{user.username}</Table.Cell>
+						<Table.Cell>{language}</Table.Cell>
 						<Table.Cell>
-							{new Intl.DateTimeFormat("en-uk").format(dayjs(submission.createdAt).toDate())}
+							<!-- todo: make this more readable for screen readers somehow -->
+							{dayjs.duration(dayjs(createdAt).diff(game.startTime)).format("HH:mm:ss")}
 						</Table.Cell>
-						<Table.Cell>{submission.result}</Table.Cell>
+						<Table.Cell>{result}</Table.Cell>
 					</Table.Row>
 				{/if}
 			{/each}
