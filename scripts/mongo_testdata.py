79e23c3438c96dd2972f201b3f5f6e0e1ca00f15
Andriamanitra <mikko77792@gmail.com>
2025-02-08 07:48:10 +0200

    update testdata script

--- scripts/mongo_testdata.py
+++ scripts/mongo_testdata.py
@@ -10,22 +10,23 @@ from pymongo import MongoClient
 
 host = os.getenv("MONGO_HOST", "127.0.0.1")
 port = int(os.getenv("MONGO_PORT", "27017"))
+db_name = os.getenv("MONGO_DB_NAME", "codincod-dev")
+
 client = MongoClient(host, port)
-db = client["codincod"]
+db = client[db_name]
 
 TESTUSER = {
-    "email": "dev@example.com",
-    "username": "dev",
+    "email": "codincoder@example.com",
+    "username": "codincoder",
     "password": "$2b$10$1.NkZMEH4IBVTZb4mDebzOQvgFS.U0t5cmLPpYbkcFfVTFoYXgWA6",
 }
 testuser_id = db.users.insert_one(TESTUSER).inserted_id
 
 FIZZBUZZ = {
     "title": "FizzBuzz",
-    "authorId": testuser_id,
-    "difficulty": "intermediate",
-    "visibility": "draft",
-    "tags": [],
+    "statement": 'Print numbers from N to M except for every number divisible by 3 print "Fizz", and for every number divisible by 5 print "Buzz". For numbers divisible by both 3 and 5 print "FizzBuzz".',
+    "constraints": "0 <= N < M <= 1000",
+    "author": testuser_id,
     "validators": [
         {
             "input": "1 3",
@@ -40,11 +41,41 @@ FIZZBUZZ = {
             "output": "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16",
         }
     ],
-    "constraints": "0 <= N < M <= 1000",
+    "difficulty": "intermediate",
+    "visibility": "draft",
     "solution": {
         "code": 'n, m = [int(x) for x in input().split()]\nfor i in range(n, m+1):\n    fizz = i % 3 == 0\n    buzz = i % 5 == 0\n    print("Fizz" * fizz + "Buzz" * buzz + str(i) * (not fizz and not buzz))\n',
         "language": "python"
     },
-    "statement": 'Print numbers from N to M except for every number divisible by 3 print "Fizz", and for every number divisible by 5 print "Buzz". For numbers divisible by both 3 and 5 print "FizzBuzz".'
+    "tags": [],
 }
 db.puzzles.insert_one(FIZZBUZZ)
+
+EASY = {
+    "title": "Easy",
+    "statement": 'Print 42.',
+    "constraints": "",
+    "author": testuser_id,
+    "validators": [
+        {
+            "input": "1",
+            "output": "42",
+        },
+        {
+            "input": "2",
+            "output": "42",
+        },
+        {
+            "input": "3",
+            "output": "42",
+        }
+    ],
+    "difficulty": "beginner",
+    "visibility": "approved",
+    "solution": {
+        "code": 'print(42)',
+        "language": "python"
+    },
+    "tags": [],
+}
+db.puzzles.insert_one(EASY)
