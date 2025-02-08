2949fe49a0684313bfec44c08ebb6a46360af732
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-02-08 03:02:43 +0100

    feat: improve game submission endpoint

---
+++ libs/types/src/core/submission/config/game-submission-params.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { objectIdSchema } from "../../common/schema/object-id.js";
+
+export const gameSubmissionParamsSchema = z
+	.object({
+		gameId: objectIdSchema,
+		submissionId: objectIdSchema,
+		userId: objectIdSchema
+	})
+	.required();
+
+export type GameSubmissionParams = z.infer<typeof gameSubmissionParamsSchema>;
