40d467462bbac7da12fa6fcd62245750ed148313
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-02-11 23:48:20 +0100

    fix: improve readability of paginatedQuerySchema

--- libs/types/src/core/common/schema/paginated-query.schema.ts
+++ libs/types/src/core/common/schema/paginated-query.schema.ts
@@ -1,16 +1,8 @@
 import { z } from "zod";
 
-const numericString = z
-	.string()
-	.regex(/^\d+$/, "Must be a numeric string")
-	.transform(Number)
-	.pipe(z.number());
-
-const positiveNumberSchema = z.union([z.number(), numericString]).transform(Number);
-
 export const paginatedQuerySchema = z.object({
-	page: positiveNumberSchema.pipe(z.number().min(1)).default(1),
-	pageSize: positiveNumberSchema.pipe(z.number().min(1).max(100)).default(10)
+	page: z.coerce.number().min(1).default(1),
+	pageSize: z.coerce.number().min(1).max(100).default(10)
 });
 
 export type PaginatedQuery = z.infer<typeof paginatedQuerySchema>;
