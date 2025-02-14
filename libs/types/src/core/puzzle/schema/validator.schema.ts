fcb2103124799a26245b71d1c7f621ce001759cb
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-02-14 04:31:26 +0100

    fix: use accepted date schema

--- libs/types/src/core/puzzle/schema/validator.schema.ts
+++ libs/types/src/core/puzzle/schema/validator.schema.ts
@@ -1,17 +1,12 @@
 import { z } from "zod";
 import { pistonExecutionResponseSuccessSchema } from "../../piston/schema/execution-response/execution-success.js";
+import { acceptedDateSchema } from "../../common/schema/accepted-date.js";
 
 export const validatorEntitySchema = z.object({
 	input: z.string().transform((s) => s.trimEnd()),
 	output: z.string().transform((s) => s.trimEnd()),
-	createdAt: z
-		.date()
-		.default(() => new Date())
-		.optional(),
-	updatedAt: z
-		.date()
-		.default(() => new Date())
-		.optional(),
+	createdAt: acceptedDateSchema,
+	updatedAt: acceptedDateSchema,
 	testResult: pistonExecutionResponseSuccessSchema.pick({ compile: true, run: true }).optional()
 });
 export type ValidatorEntity = z.infer<typeof validatorEntitySchema>;
