51b9c55ca0959ec095684bd6efbb8738ca9c2a3f
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-02-14 04:29:39 +0100

    fix: use correct schemas in submissionentityschema

--- libs/types/src/core/submission/schema/submission-entity.schema.ts
+++ libs/types/src/core/submission/schema/submission-entity.schema.ts
@@ -4,14 +4,15 @@ import { acceptedDateSchema } from "../../common/schema/accepted-date.js";
 import { userDtoSchema } from "../../user/schema/user-dto.schema.js";
 import { puzzleLanguage, puzzleLanguageVersion } from "../../puzzle/schema/puzzle-language.js";
 import { puzzleDtoSchema } from "../../puzzle/schema/puzzle-dto.schema.js";
+import { objectIdSchema } from "../../common/schema/object-id.js";
 
 export const submissionEntitySchema = z.object({
 	code: z.string().optional(),
 	language: puzzleLanguage,
 	languageVersion: puzzleLanguageVersion,
 	createdAt: acceptedDateSchema.default(() => new Date()),
-	puzzle: z.string().or(puzzleDtoSchema),
+	puzzle: objectIdSchema.or(puzzleDtoSchema),
 	result: puzzleResultSchema,
-	user: z.string().or(userDtoSchema)
+	user: objectIdSchema.or(userDtoSchema)
 });
 export type SubmissionEntity = z.infer<typeof submissionEntitySchema>;
