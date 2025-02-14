18ea54f2a5b3dc334e511ba40ce0fbcba76a0c8b
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-02-14 04:30:27 +0100

    fix: issue with userId sometimes being ObjectId

--- libs/types/src/core/common/schema/object-id.ts
+++ libs/types/src/core/common/schema/object-id.ts
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 import { z } from "zod";
 
-export const objectIdSchema = z.string().refine((val) => {
-	return mongoose.Types.ObjectId.isValid(val);
-});
+export const objectIdSchema = z.preprocess(
+	(val) => {
+		if (val instanceof mongoose.Types.ObjectId) {
+			return val.toString();
+		}
+		return val;
+	},
+	z.string().refine((val) => {
+		return mongoose.Types.ObjectId.isValid(val);
+	})
+);
 
 export type ObjectId = z.infer<typeof objectIdSchema>;
