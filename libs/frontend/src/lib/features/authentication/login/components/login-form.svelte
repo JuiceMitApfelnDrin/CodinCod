253ebd149a0ee35429e2db0e188d8b939cbd6c2b
J Mad <36441093+reeveng@users.noreply.github.com>
2025-03-11 18:31:36 +0000

    Fix login issue forgotton import of isemail (#127)
    * fix: login issue, only taken username length into account
    
    * fix: login, forgotton import of isemail
    
    * Apply automatic changes

--- libs/frontend/src/lib/features/authentication/login/components/login-form.svelte
+++ libs/frontend/src/lib/features/authentication/login/components/login-form.svelte
@@ -4,14 +4,7 @@
 	import { Input } from "@/components/ui/input";
 	import { zodClient } from "sveltekit-superforms/adapters";
 	import { debounce } from "@/utils/debounce";
-	import {
-		loginSchema,
-		PASSWORD_CONFIG,
-		POST,
-		USERNAME_CONFIG,
-		IDENTIFIER_CONFIG,
-		type Login
-	} from "types";
+	import { loginSchema, PASSWORD_CONFIG, POST, IDENTIFIER_CONFIG, type Login } from "types";
 	import GenericAlert from "@/components/ui/alert/generic-alert.svelte";
 	import { isHttpErrorCode } from "@/utils/is-http-error-code";
 	import { page } from "$app/stores";
