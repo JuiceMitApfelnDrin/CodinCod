f7e53e14a01e99b42d228e2b1de231a08a854b84
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-03-21 18:20:26 +0100

    feat: add docs/activity page

---
+++ libs/frontend/src/routes/docs/activity/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import WorkInProgress from "@/components/status/work-in-progress.svelte";
+	import P from "@/components/typography/p.svelte";
+	import Container from "@/components/ui/container/container.svelte";
+</script>
+
+<Container>
+	<P>
+		<WorkInProgress />
+		Activity can be seen as the following things:
+	</P>
+	<ul class="list-inside list-disc">
+		<li>A solved puzzle</li>
+		<li>Creating a puzzle</li>
+	</ul>
+</Container>
