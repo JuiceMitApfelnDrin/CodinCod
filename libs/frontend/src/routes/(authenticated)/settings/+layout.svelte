65d0cc8531b8933aef628ada37743da5c605a061
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-03-25 22:59:03 +0100

    refactor: make generic side-navigation and move side-navigation to layout for /settings/*

---
+++ libs/frontend/src/routes/(authenticated)/settings/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+	import SideNavigationItem from "@/components/nav/side-navigation/side-navigation-item.svelte";
+	import SideNavigation from "@/components/nav/side-navigation/side-navigation.svelte";
+	import Container from "@/components/ui/container/container.svelte";
+	import LogicalUnit from "@/components/ui/logical-unit/logical-unit.svelte";
+
+	import { Bell, MessageCircle, Paintbrush2, Settings, Settings2, User } from "lucide-svelte";
+	import { frontendUrls } from "types";
+</script>
+
+<Container class="flex md:flex-row">
+	<SideNavigation>
+		<SideNavigationItem href={frontendUrls.SETTINGS_PROFILE} icon={User} text="Edit profile" />
+		<SideNavigationItem
+			href={frontendUrls.SETTINGS_PREFERENCES}
+			icon={Settings2}
+			text="Preferences"
+		/>
+		<SideNavigationItem
+			href={frontendUrls.SETTINGS_APPEARANCE}
+			icon={Paintbrush2}
+			text="Appearance"
+		/>
+		<SideNavigationItem
+			href={frontendUrls.SETTINGS_COMMUNITY}
+			icon={MessageCircle}
+			text="Community"
+		/>
+		<SideNavigationItem
+			href={frontendUrls.SETTINGS_NOTIFICATIONS}
+			icon={Bell}
+			text="Notifications"
+		/>
+		<SideNavigationItem href={frontendUrls.SETTINGS_ACCOUNT} icon={Settings} text="Account" />
+	</SideNavigation>
+
+	<LogicalUnit>
+		<slot />
+	</LogicalUnit>
+</Container>
