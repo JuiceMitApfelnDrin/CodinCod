37b29c6ce4a055bdc05175088c20d5f9166508f3
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2025-04-06 00:46:24 +0200

    cleanup: remove unused imports

--- libs/backend/src/index.ts
+++ libs/backend/src/index.ts
@@ -1,6 +1,6 @@
 import server from "./app.js";
 
-const FASTIFY_HOST = process.env.FASTIFY_HOST || "0.0.0.0";
+const FASTIFY_HOST = process.env.FASTIFY_HOST ?? "0.0.0.0";
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 8888;
 
 // start server
